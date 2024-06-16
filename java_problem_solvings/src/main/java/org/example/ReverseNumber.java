9514e514a779fca9e8c74a2a2a645f16d74c083c
srsohan <srsohan96@gmail.com>
2024-06-16 22:12:11 +0600

    Add existing project files to Git

---
+++ java_problem_solvings/src/main/java/org/example/ReverseNumber.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        System.out.println("ReverseNumber.main");
+    }
+}
---
+++ java_problem_solvings/src/main/java/org/example/Swap2Numbers.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Swap2Numbers
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
